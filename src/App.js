98148e6e1adf21cb7d0d14a36cf5b5acb5f78ec7
radiyars <radiyars@yandex.ru>
2023-08-16 13:13:08 +0300

    длобавлен запрос продукции на сервер mocapi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useEffect, useState } from "react";
 import "./scss/app.scss";
 import Categories from "./сomponents/Categories";
 import Header from "./сomponents/Header";
 import PizzaBlock from "./сomponents/PizzaBlock";
 import Sort from "./сomponents/Sort";
-import React from "react";
-import pizzas from "./assets/pizzas.json";
 
 function App() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://64dc883ce64a8525a0f6a48c.mockapi.io/items")
+            .then((res) => {
+                return res.json();
+            })
+            .then((arr) => {
+                setItems(arr);
+            });
+    }, []);
+
     return (
         <div className="wrapper">
             <Header />
@@ -18,7 +29,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => (
+                        {items.map((obj) => (
                             <PizzaBlock key={obj.id} {...obj} />
                         ))}
                     </div>
