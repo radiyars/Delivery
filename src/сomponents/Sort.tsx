e6cf1bde20857f68eb46dd0516608b31acc567a6
Radiy <radiyars@yandex.ru>
2023-10-05 17:57:21 +0300

    перевел в tsx: Sort, Header, CartEmpty

--- src/сomponents/Sort.tsx
+++ src/сomponents/Sort.tsx
@@ -1,29 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectSort, setSortType } from "../redux/slices/filterSlice";
+import { ReactComponent as ArrowTopSvg } from "./../assets/img/arrow-top.svg";
 
-export const sortList = [
+type SortItemType = {
+  name: string;
+  sortProperty: string;
+};
+
+export const sortList: SortItemType[] = [
   { name: "популярности", sortProperty: "rating" },
   { name: "цене", sortProperty: "price" },
-  { name: "алфавиту", sosortPropertyrt: "title" },
+  { name: "алфавиту", sortProperty: "title" },
 ];
 
 const Sort = () => {
   const dispatch = useDispatch();
   const sortType = useSelector(selectSort);
   const [isOpenPopup, setIsOpenPopup] = useState(false);
-  const sortRef = useRef();
+  const sortRef = useRef<HTMLDivElement>(null);
 
-  const onClickSortItem = (obj) => {
+  const onClickSortItem = (obj: SortItemType) => {
     dispatch(setSortType(obj));
     setIsOpenPopup(false);
   };
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
+    const handleClickOutside = (event: any) => {
       if (!event.composedPath().includes(sortRef.current)) {
         setIsOpenPopup(false);
-        console.log("click!");
       }
     };
 
@@ -35,18 +40,7 @@ const Sort = () => {
   return (
     <div ref={sortRef} className="sort">
       <div className="sort__label">
-        <svg
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
+        <ArrowTopSvg />
         <b>Сортировка по:</b>
         <span
           onClick={() => {
